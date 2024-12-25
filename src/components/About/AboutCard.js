06503e3ccee2a4d03a38a55b6a3dbeca1f684248
Gurpreetsingh4547 <gu07869prince@gmail.com>
2024-12-25 21:21:35 +0530

    Implement about page of portfolio.

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -23,7 +23,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading Tech Blogs
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,14 +5,14 @@ import {
   SiPostman,
   SiSlack,
   SiVercel,
-  SiMacos,
+  SiWindows95,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows95 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
