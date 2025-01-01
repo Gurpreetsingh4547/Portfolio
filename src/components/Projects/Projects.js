48009e2d935ee3985026ec6b68e5f0f83e5c17be
Gurpreetsingh4547 <gu07869prince@gmail.com>
2025-01-01 15:25:06 +0530

    Add my more projects.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,8 @@ import Particle from "../Particle";
 import projectPeak from "../../Assets/Projects/project-peak.png";
 import jobprogress from "../../Assets/Projects/jobprogress.png";
 import sarthi from "../../Assets/Projects/sarthi.png";
+import restaurent from "../../Assets/Projects/restaurant.png";
+import ecommerce from "../../Assets/Projects/ecommerce.png";
 
 function Projects() {
   return (
@@ -47,6 +49,28 @@ function Projects() {
               description="Project Peak is an intuitive project management tool designed to streamline workflows. Users can effortlessly create projects, update their statuses, and monitor progress through dynamic graphs. The platform offers detailed insights, including pending and completed tasks and timelines for project creation and completion. Additionally, Project Peak keeps users informed by showcasing their most recent projects, ensuring enhanced productivity and effective tracking."
               ghLink="https://github.com/Gurpreetsingh4547/projectpeak"
               demoLink="https://projectpeak-4.onrender.com/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={restaurent}
+              isBlog={false}
+              title="Canadian Restaurant"
+              description="A sleek, modern website for a Canadian-themed restaurant, featuring a welcoming homepage, an About Us section introducing Canadian dishes, and a menu with food items, prices, and easy order options. The design uses dark themes with golden accents for a premium feel and includes a reservation feature for customer convenience."
+              ghLink="https://github.com/Gurpreetsingh4547/Resturant-sass-project"
+              demoLink="https://resturant-sass-project.onrender.com/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ecommerce}
+              isBlog={false}
+              title="E-Commerce"
+              description="An e-commerce site where users can view product listings, add items to the cart, remove items from the cart, and view the cart. The site is responsive and allows easy product browsing and cart management. Admins can manage products and orders."
+              ghLink="https://github.com/Gurpreetsingh4547/E-commerce"
+              demoLink="https://e-commerce-6jp8.onrender.com/"              
             />
           </Col>
         </Row>
